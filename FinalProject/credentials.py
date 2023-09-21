bdd30db3ad5b89c31aa9253f63179b208c1548bb
OlgaPitkevica <krentale@gmail.com>
2023-09-21 11:16:24 +0300

    update

---
+++ FinalProject/credentials.py
@@ -0,0 +1,8 @@
+client_id = "a1a8dafef9614bb68dcc729cd14a3399"
+client_secret = "8ad8630c3bc04d47ab3839cf106cd324"
+uri_mongo = "mongodb+srv://krentale:wEJqwQN8LjAqO56e@cluster0.bchpjdz.mongodb.net/?retryWrites=true&w=majority"
+
+
+uri_neo4j = "neo4j+ssc://14c452fc.databases.neo4j.io"
+username = "neo4j"
+password = "5sp0oCaFHPfmkckEPVLKb8Kg1rgbj7FuFN5HBH710YE"
